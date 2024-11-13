7a7bc58f1c6e5003c138cef2e11c9edbadeef47d
MohamedMoustir <itsmoustir@gmail.com>
2024-11-13 02:27:25 +0100

    add tag html in js

--- assets/js/M_main.js
+++ assets/js/M_main.js
@@ -17,19 +17,19 @@ carousel_cards.addEventListener("wheel", (evnt) => {
 
 //slide-images catgory
 
-const carousel_cards1 = document.getElementById('carousel-cards1');
+const carousel_cards_words = document.getElementById('carousel-cards_Words');
 document.getElementById("1").style.background = "#608BC1"
 document.getElementById("rightBtn").addEventListener("click", () => {
 
-  carousel_cards1.style.scrollBehavior = "smooth"
-  carousel_cards1.scrollLeft += 1500;
+  carousel_cards_words.style.scrollBehavior = "smooth"
+  carousel_cards_words.scrollLeft += 1500;
 
 
-  if (carousel_cards1.scrollLeft < 1500) {
+  if (carousel_cards_words.scrollLeft < 1500) {
     document.getElementById("2").style.background = "#608BC1"
     document.getElementById("1").style.background = "none"
 
-  } else if (carousel_cards1.scrollLeft >= 1500) {
+  } else if (carousel_cards_words.scrollLeft >= 1500) {
     document.getElementById("2").style.background = "none"
     document.getElementById("3").style.background = "#608BC1"
 
@@ -40,14 +40,14 @@ document.getElementById("rightBtn").addEventListener("click", () => {
 
 
 document.getElementById("leftBtn").addEventListener("click", () => {
-  carousel_cards1.style.scrollBehavior = "smooth"
-  carousel_cards1.scrollLeft -= 1500;
+  carousel_cards_words.style.scrollBehavior = "smooth"
+  carousel_cards_words.scrollLeft -= 1500;
 
-  if (carousel_cards1.scrollLeft > 1500) {
+  if (carousel_cards_words.scrollLeft > 1500) {
     document.getElementById("2").style.background = "#608BC1"
     document.getElementById("3").style.background = "none"
 
-  } else if (carousel_cards1.scrollLeft <= 1500) {
+  } else if (carousel_cards_words.scrollLeft <= 1500) {
     document.getElementById("2").style.background = "none"
     document.getElementById("1").style.background = "#608BC1"
 
@@ -61,15 +61,15 @@ function slideNum(id) {
 
   if (id == "1") {
     document.getElementById("1").style.background = "#608BC1"
-    carousel_cards1.style.scrollBehavior = "smooth"
+    carousel_cards_words.style.scrollBehavior = "smooth"
     document.getElementById("2").style.background = "none"
     document.getElementById("3").style.background = "none"
   }
 
   if (id == "2") {
     document.getElementById("2").style.background = "#608BC1"
-    carousel_cards1.style.scrollBehavior = "smooth"
-    carousel_cards1.scrollLeft += 1500;
+    carousel_cards_words.style.scrollBehavior = "smooth"
+    carousel_cards_words.scrollLeft += 1500;
     document.getElementById("1").style.background = "none"
     document.getElementById("3").style.background = "none"
 
@@ -78,8 +78,8 @@ function slideNum(id) {
 
   if (id == "3") {
     document.getElementById("3").style.background = "#608BC1"
-    carousel_cards1.style.scrollBehavior = "smooth"
-    carousel_cards1.scrollLeft += 3000;
+    carousel_cards_words.scrollBehavior = "smooth"
+    carousel_cards_words.scrollLeft += 3000;
     document.getElementById("1").style.background = "none"
     document.getElementById("2").style.background = "none"
 
@@ -90,12 +90,13 @@ function slideNum(id) {
 //request restApi get post delete patch put ..
 //responce data array 
 //option methode status:200 - 404 - 500 - 403
-  fetch("https://mohamedmoustir.github.io/api/")
+
+fetch("https://mohamedmoustir.github.io/api/")
   .then(result => result.json())
-  .then(function(data){
+  .then(function (data) {
     for (let i = 0; i < data.Tshorts.length; i++) {
-     let cloths = data.Tshorts
-     carousel_cards.innerHTML +=`
+      let cloths = data.Tshorts
+      carousel_cards.innerHTML += `
         
      <div class=" mx-8 max-w-sm bg-white border border-gray-200 rounded-lg shadow max-h-[600px]">
                  <a href="#">
@@ -137,7 +138,7 @@ function slideNum(id) {
                              class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
                      </div>
                      <div class="flex items-center justify-between">
-                         <span class="text-3xl font-bold text-gray-900 dark:text-white">${cloths[i].price}</span>
+                         <span class="text-3xl font-bold text-gray-900 dark:text-white">${cloths[i].price}$</span>
                          <a href="#"
                              class="  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  ">Add
                              to cart</a>
@@ -145,6 +146,185 @@ function slideNum(id) {
                  </div>
              </div>
      
-     `     
+     `
+    }
+  })
+
+
+fetch("https://mohamedmoustir.github.io/api/")
+  .then(result => result.json())
+  .then(function (data) {
+    const carousel_cards_words = document.getElementById('carousel-cards_Words');
+    for (let i = 0; i < 3; i++) {
+      let cloths = data.Tshorts
+      carousel_cards_words.innerHTML += `
+     
+     
+     <div class="grid sm:grid-cols-2 xl:grid-cols-3 gap-6 justify-evenly m-[100px]">
+
+                <div
+                    class="max-w-sm w-[90%] h-52 min-w-[250px] border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-[150px]">
+                    <a href="#">
+                        <img class="rounded-t-lg w-full h-52" src="../assets/imgs/home/video.png" alt="" />
+                    </a>
+                    <div class="p-5  h-auto  shadow">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white w-full">
+                                Noteworthy</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise
+                            technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <a href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+
+
+                <div
+                    class="max-w-sm w-[90%] min-w-[250px] h-52 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-[150px]">
+                    <a href="#">
+                        <img class="rounded-t-lg w-full h-52" src="../assets/imgs/home/video.png" alt="" />
+                    </a>
+                    <div class="p-5  h-auto  shadow">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white w-full">
+                                Noteworthy</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise
+                            technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <a href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+
+
+
+                <div
+                    class="max-w-sm w-[90%] min-w-[250px] h-52 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-[150px]">
+                    <a href="#">
+                        <img class="rounded-t-lg w-full h-52" src="../assets/imgs/home/video.png" alt="" />
+                    </a>
+                    <div class="p-5  h-auto  shadow">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white w-full">
+                                Noteworthy</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise
+                            technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <a href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+
+
+
+
+                <div
+                    class="max-w-sm w-[90%] min-w-[250px] h-52 min-w-[250px] border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-[150px]">
+                    <a href="#">
+                        <img class="rounded-t-lg w-full h-52" src="../assets/imgs/home/video.png" alt="" />
+                    </a>
+                    <div class="p-5  h-auto  shadow">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white w-full">
+                                Noteworthy</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise
+                            technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <a href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+
+
+
+                <div
+                    class="max-w-sm w-[90%] min-w-[250px] h-52 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-[150px]">
+                    <a href="#">
+                        <img class="rounded-t-lg w-full h-52" src="../assets/imgs/home/video.png" alt="" />
+                    </a>
+                    <div class="p-5  h-auto  shadow">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white w-full">
+                                Noteworthy</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise
+                            technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <a href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+
+
+
+                <div
+                    class="max-w-sm w-[90%] min-w-[250px] h-52 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-[150px]">
+                    <a href="#">
+                        <img class="rounded-t-lg w-full h-52" src="../assets/imgs/home/video.png" alt="" />
+                    </a>
+                    <div class="p-5  h-auto  shadow">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white w-full">
+                                Noteworthy</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise
+                            technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <a href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+
+
+
+            </div>
+     
+     
+     `
+
+
+
+
     }
   })

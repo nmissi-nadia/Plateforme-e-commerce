5ad942c14468db550cced4eb271b641b676f0aad
oussamaamou <amoussdark@gmail.com>
2024-11-15 16:25:53 +0100

    L'Ajout de la caroussel de personnalisation

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,3 +1,89 @@
+/** Consommation de l'API */
+const apiUrl = 'https://mohamedmoustir.github.io/api/';
+let datatch = [];
+let id = location.href.split("?")[1].split("=")[1];
+
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(function (data){
+    datatch.push(data);
+
+});
+
+
+
+
+
+
+function afficherProduit() {
+
+
+};
+
+
+
+
+
+
+
+psnlcard.addEventListener('submit', () => {
+
+    stockerdata();
+
+});
+
+
+
+
+
+let dataprdts = JSON.parse(localStorage.getItem('produits')) || [];
+
+function stockerdata() {
+
+    let prdt = {
+        
+        taille: taislct.value,
+        couleur: chxclrprdt.value,
+        nombre: vleprd.value,
+        identifiant: id
+    };
+
+    dataprdts.push(prdt);
+
+    localStorage.setItem('produits', JSON.stringify(dataprdts));
+
+   
+};
+console.log(dataprdts); 
+
+
+
+
+/**////////////////////////////////////////////////////// */
+
+
+
+
+/** Affichage des images et des informations */
+
+
+
+/** Declaration des variables et des constantes */
+let spneqpnm = document.querySelectorAll(' .spneqpnm');
+const tailleprdt = document.getElementById("tailleprdt");
+const dcrptprdt = document.getElementById("dcrptprdt");
+const cltrprdt = document.getElementById("cltrprdt");
+const payprdt = document.getElementById("payprdt");
+
+
+
+
+
+const taislct = document.getElementById("taislct");
+const chxclrprdt = document.getElementById("chxclrprdt");
+
+
+/**////////////////////////////////////////////////////// */
 
 
 
@@ -45,7 +131,39 @@ chldimg.forEach((img) => {
 });
 
 
+/**////////////////////////////////////////////////////// */
 
 
+
+/** Caroussel des images */
+
+const crslimgs = document.querySelectorAll('.fdrimg');
+const bttnagch = document.getElementById("btnlft");
+const bttndrt = document.getElementById("btnrgt");
+
+let currentIndex = 0;
+
+function updateCarousel() {
+    crslimgs.forEach((img, index) => {
+        if (index === currentIndex) {
+            img.classList.remove('hidden'); 
+        } else {
+            img.classList.add('hidden');
+        }
+    });
+}
+
+bttnagch.addEventListener('click', () => {
+    currentIndex = currentIndex === 0 ? crslimgs.length - 1 : currentIndex - 1;
+    updateCarousel();
+});
+
+bttndrt.addEventListener('click', () => {
+    currentIndex = currentIndex === crslimgs.length - 1 ? 0 : currentIndex + 1;
+    updateCarousel();
+});
+
+updateCarousel();
 
 /**////////////////////////////////////////////////////// */
+

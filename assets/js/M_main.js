0943ce97c3947e251e42a9757f7431455db958f9
MohamedMoustir <itsmoustir@gmail.com>
2024-11-16 14:51:10 +0100

    added style

--- assets/js/M_main.js
+++ assets/js/M_main.js
@@ -239,7 +239,6 @@ fetch("https://mohamedmoustir.github.io/nweapi/")
         for (let i = 0; i < 3; i++) {
             let worldfot = data.worldfot
 
-
             carousel_cards_words.innerHTML += `
      
      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center mx-auto my-[100px]">
@@ -254,7 +253,7 @@ fetch("https://mohamedmoustir.github.io/nweapi/")
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white w-full">
                                ${worldfot[0].name}</h5>
                         </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> ${worldfot[0].description.slice(0, 100)}.</p>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> ${worldfot[0].description.slice(0, 100)}...</p>
                         <a href="#"
                             class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Read more
@@ -406,7 +405,7 @@ document.getElementById("close-btn").addEventListener("click", function () {
     panelCard.classList.add("hidden");
 });
 
-const testimnal= document.getElementById("testimnal")
+const testimnal = document.getElementById("testimnal")
 function NextSlide() {
     testimnal.style.scrollBehavior = "smooth"
     testimnal.scrollLeft += 500;

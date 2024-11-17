ea383dda245b77639bba3a7f9c96258a863eacaf
nmissi-nadia <nmissinadia@gmail.com>
2024-11-17 17:05:33 +0100

    configuration des panier

--- assets/js/abdo.js
+++ assets/js/abdo.js
@@ -14,18 +14,29 @@ function toggleAnswer(answerId) {
 }
 
 // Fonction d'Afficher et cacher aside barre
-document.getElementById("shoping-icon-md").addEventListener("click", function () {
-  const shoppingIconMd = document.getElementById("shoping-icon-md");
-  const panelCard = document.getElementById("panel-aside-bar");
+const panelIcons = document.querySelectorAll(".panel-icons");
 
-  panelCard.classList.remove("hidden");
-})
-
-document.getElementById("close-btn").addEventListener("click", function () {
-  const panelCard = document.getElementById("panel-aside-bar");
-
-  panelCard.classList.add("hidden");
+panelIcons.forEach(function(icon) {
+  icon.addEventListener("click", function () {
+    const panelCard = document.getElementById("panel-aside-bar");
+    displayCartItems();
+    panelCard.classList.remove("hidden");
+  });
 });
+
+    document.getElementById("close-btn").addEventListener("click", function () {
+      const panelCard = document.getElementById("panel-aside-bar");
+
+      panelCard.classList.add("hidden");
+    })
+
+    const seePanel = document.getElementById("see-panel");
+    seePanel.addEventListener("click", function () {
+      const panelCard = document.getElementById("panel-aside-bar");
+      panelCard.classList.add("hidden");
+      window.location.href = "../vues/panier.html";
+
+    })
 
 
 
--- assets/js/catalo.js
+++ assets/js/catalo.js
@@ -248,15 +248,26 @@ searchButton.addEventListener('click', () => {
   }
 });
 // Fonction d'Afficher et cacher aside barre
-document.getElementById("shoping-icon-md").addEventListener("click", function () {
-    const shoppingIconMd = document.getElementById("shoping-icon-md");
-    const panelCard = document.getElementById("panel-aside-bar");
+const panelIcons = document.querySelectorAll(".panel-icons");
 
+panelIcons.forEach(function(icon) {
+  icon.addEventListener("click", function () {
+    const panelCard = document.getElementById("panel-aside-bar");
+    displayCartItems();
     panelCard.classList.remove("hidden");
-  })
-
-  document.getElementById("close-btn").addEventListener("click", function () {
-    const panelCard = document.getElementById("panel-aside-bar");
-
-    panelCard.classList.add("hidden");
   });
+});
+
+    document.getElementById("close-btn").addEventListener("click", function () {
+      const panelCard = document.getElementById("panel-aside-bar");
+
+      panelCard.classList.add("hidden");
+    })
+
+    const seePanel = document.getElementById("see-panel");
+    seePanel.addEventListener("click", function () {
+      const panelCard = document.getElementById("panel-aside-bar");
+      panelCard.classList.add("hidden");
+      window.location.href = "../vues/panier.html";
+
+    })

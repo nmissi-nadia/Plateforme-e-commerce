6ddd1b83c1e54f9762730596f4eb340f2e15745e
nmissi-nadia <nmissinadia@gmail.com>
2024-11-17 15:21:37 +0100

    change function add

--- assets/js/catalo.js
+++ assets/js/catalo.js
@@ -117,11 +117,12 @@ function toggleSidebar(show) {
 function ajouteraupanier(id, title, price, image) {
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
     const existingProduct = cart.find(item => item.id === id);
-  
+    let L ='large';
+    let couleur ='Domicile';
     if (existingProduct) {
       existingProduct.quantity += 1;
     } else {
-      cart.push({ id, name: title, price, image, quantity: 1 });
+      cart.push({ id, name: title, price, couleur, quantity: 1,taille:L });
     }
   
     localStorage.setItem('cart', JSON.stringify(cart));
@@ -207,17 +208,28 @@ function afficherProduitsFiltres(produits) {
   produits.forEach(produit => {
     const produitCard = `
       <div class="max-w-xs bg-white rounded-lg shadow-md overflow-hidden group relative z-10">
-        <div class="relative">
-          <img src="${produit.images[0]}" alt="${produit.title}" class="w-full">
-        </div>
-        <div class="p-4">
-          <h3 class="text-lg font-semibold text-gray-800">${produit.title}</h3>
-          <p class="text-sm text-gray-500">${produit.description}</p>
-          <div class="flex items-center space-x-2 mt-2">
-            <span class="text-lg font-bold text-gray-900">${produit.price} MAD</span>
+          <div class="relative">
+            <img src="${produit.images[2]}" alt="${produit.title}" class="w-full">
+            <span class="absolute top-2 right-2 bg-red-500 text-white text-xs font-bold py-1 px-2 rounded-full">-50%</span>
+          </div>
+          <div class="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <button class="bg-white text-orange-500 font-semibold py-2 px-4 rounded-md mb-4" onclick="ajouteraupanier('${produit.id}','${produit.title}','${produit.price}','${produit.images[0]}')">
+              Ajouter au panier
+              <i class="fa-solid fa-cart-shopping"></i>
+            </button>
+            <button class="bg-white text-orange-500 font-semibold py-2 px-4 rounded-md mb-4">
+              <a href="../vues/details.html?id=${produit.id}">View Details</a>
+            </button>
+          </div>
+          <div class="p-4">
+            <h3 class="text-lg font-semibold text-gray-800">${produit.title}</h3>
+            <p class="text-sm text-gray-500">${produit.description}</p>
+            <div class="flex items-center space-x-2 mt-2">
+              <span class="text-lg font-bold text-gray-900">${produit.price} MAD</span>
+              <span class="text-sm text-gray-400 line-through">${(produit.price * 2).toFixed(2)} MAD</span>
+            </div>
           </div>
         </div>
-      </div>
     `;
     produitContainer.insertAdjacentHTML('beforeend', produitCard);
   });

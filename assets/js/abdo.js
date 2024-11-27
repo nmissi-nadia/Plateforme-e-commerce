877e4d8c34dec6fb21017c6c5b89b6a921f67642
nmissi-nadia <nmissinadia@gmail.com>
2024-11-27 20:00:46 +0100

    before the merge

--- assets/js/abdo.js
+++ assets/js/abdo.js
@@ -1,21 +1,4 @@
-function toggleAnswer(answerId) {
-  const answerElement = document.getElementById(answerId);
-  const icon = answerElement.previousElementSibling.querySelector("svg");
 
-  // Toggle the display of the answer
-  answerElement.classList.toggle('hidden');
-       
-  // Rotate the icon when the answer is shown
-  if (answerElement.classList.contains('hidden')) {
-    icon.style.transform = "rotate(0deg)";
-  } else {
-    icon.style.transform = "rotate(90deg)";
-  }
-}
-
-
-
-// ----------------------------------------------------------------------------------------------
 // Fonction d'Afficher et cacher aside barre
 const panelIcons = document.querySelectorAll(".panel-icons");
 
@@ -107,4 +90,44 @@ function displayCartItems() {
   });
 
   totalPriceDiv.innerHTML =` <p>TOTAL PANIER : ${totalPrice} MAD</p>`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function toggleAnswer(answerId) {
+  const answerElement = document.getElementById(answerId);
+  const icon = answerElement.previousElementSibling.querySelector("svg");
+
+  // Toggle the display of the answer
+  answerElement.classList.toggle('hidden');
+       
+  // Rotate the icon when the answer is shown
+  if (answerElement.classList.contains('hidden')) {
+    icon.style.transform = "rotate(0deg)";
+  } else {
+    icon.style.transform = "rotate(90deg)";
+  }
 }
